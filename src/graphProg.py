54279052e72c5f0c928207d6f40dd1a4bc856381
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-05-18 10:15:41 -0500

    higiene

--- src/graphProg.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-
-
-dat = np.loadtxt("progreso.dat", delimiter = " ")
-
-plt.plot(dat[:,1])
-plt.show()
-plt.close()
--- src/try.py
+++
@@ -1,5 +0,0 @@
-from numpy.random import choice
-z = [1,2,3]
-b =3 
-a = [(b-x + 0.0)/(b*(b+1)/2) for x in range(b)]
-print choice(z,p=[1.0/3,1.0/3,1.0/3])
